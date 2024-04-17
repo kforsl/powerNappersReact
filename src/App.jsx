ad0845cadbe09e8c4c28ecaa2ff21d8d9b4e3133
kforsl <forslund95@live.com>
2024-04-17 16:47:12 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,44 @@ import SearchPage from "./pages/searchPage/SearchPage.jsx"
 import FavoritesPage from './pages/favoritesPage/FavoritesPage';
 import WatchListPage from './pages/watchListPage/WatchListPage';
 import SingleMoviePage from './pages/singleMoviePage/SingleMoviePage';
+import { useState } from "react"
 
 function App() {
 
-  return (
-    <div>Powernappers
-      <Header />
-      <Routes>
-        <Route path="/" element={ <HomePage /> } />
-        <Route path="/search/:id" element={ <SearchPage /> } />
-        <Route path="/watchlist/" element={ <WatchListPage />} />
-        <Route path="/favorites/" element={ <FavoritesPage />} />
-        <Route path="/singlemovie/:id" element={ <SingleMoviePage /> } />
-      </Routes>
-      <Footer />
-    </div>
-  )
+    const [recentlyViewed, setRecentlyViewed] = useState([])
+
+    const handleRecentlyViewed = (movie) => {
+        console.log(typeof (movie));
+        if (movie === 'clear') {
+            console.log('true');
+            setRecentlyViewed([])
+        } else {
+            console.log('false');
+            const newClickedMovie = { ...movie }
+            const newRecentlyViewed = []
+            const filterdRecently = recentlyViewed.filter(m => m.imdbID !== newClickedMovie.imdbID)
+            for (let i = 0; i < 5; i++) {
+                filterdRecently[i] && newRecentlyViewed.push(filterdRecently[i])
+            }
+            newRecentlyViewed.unshift(newClickedMovie)
+            setRecentlyViewed(newRecentlyViewed)
+        }
+    }
+
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/search/:id" element={<SearchPage />} />
+                <Route path="/watchlist/" element={<WatchListPage />} />
+                <Route path="/favorites/" element={<FavoritesPage />} />
+                <Route path="/singlemovie/:id" element={<SingleMoviePage
+                    handleRecentlyViewed={handleRecentlyViewed} />} />
+            </Routes>
+            <Footer recentlyViewed={recentlyViewed} handleRecentlyViewed={handleRecentlyViewed} />
+        </div>
+    )
 }
 
 export default App
