83c7c8022d1c3737655a819e926c7b532dcbb39b
Lennstorm <4824136@gmail.com>
2024-04-19 12:26:54 +0200

    movielistgrid och card färdigt

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,35 @@ import HomePage from "./pages/homePage/HomePage.jsx"
 import SearchPage from "./pages/searchPage/SearchPage.jsx"
 import FavoritesPage from './pages/favoritesPage/FavoritesPage';
 import WatchListPage from './pages/watchListPage/WatchListPage';
+import { useState } from "react"
 
 function App() {
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([])
+
+  const handleFavorites = () => {
+    console.log('handleFavorites');
+  }
+  const handleWatchlist = () => {
+    console.log('handleWatchlist');
+  }
 
   return (
     <div>Powernappers
       <Header />
       <Routes>
-        <Route path="/" element={ <HomePage /> } />
-        <Route path="/search/:id" element={ <SearchPage /> } />
-        <Route path="/watchlist/" element={ <WatchListPage />} />
-        <Route path="/favorites/" element={ <FavoritesPage />} />
+        <Route path="/" element={
+          <HomePage
+            favoriteMovies={favoriteMovies}
+            watchlist={watchlist}
+            handleFavorites={handleFavorites}
+            handleWatchlist={handleWatchlist}
+          />
+        }
+        />
+        <Route path="/search/:id" element={<SearchPage />} />
+        <Route path="/watchlist/" element={<WatchListPage />} />
+        <Route path="/favorites/" element={<FavoritesPage />} />
       </Routes>
       <Footer />
     </div>
