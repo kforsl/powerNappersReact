7b93711efc65982289729f0f9ae6e9428776eab8
kforsl <forslund95@live.com>
2024-04-22 13:25:28 +0200

    tagit bort dubbla iconer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,102 +9,102 @@ const apiKey = `2799d50`
 
 function Header() {
 
-  const [searchResult, setSearchResult] = useState([])
-  const [searchValue, setSearchValue] = useState('')
+    const [searchResult, setSearchResult] = useState([])
+    const [searchValue, setSearchValue] = useState('')
 
-  const handleSearchInput = (event) => {
-    setSearchValue(event.target.value)
-    axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&s=${event.target.value}`)
-      .then(res => {
-        if (res.data.Search) {
-          setSearchResult(res.data.Search);
-        } else {
-          setSearchResult([])
-        }
-      })
-      .catch(error => console.log(error))
-  }
-
-  const handleClose = () => {
-    setSearchResult([])
-    const input = document.querySelector(`#inputField`)
-    input.value = ``
-  }
-
-  const activeBtn = () => {
-    document.querySelectorAll(`.header__list-item`)
-      .forEach(item => {
-        item.classList.remove(`active`)
-      })
-
-    if (e.target.tagName === 'LI') {
-        e.target.classList.add(`active`)
-    } else if (e.target.tagName === 'H1') {
-      document.querySelector(`.header__list-item`).classList.add(`active`)
+    const handleSearchInput = (event) => {
+        setSearchValue(event.target.value)
+        axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&s=${event.target.value}`)
+            .then(res => {
+                if (res.data.Search) {
+                    setSearchResult(res.data.Search);
+                } else {
+                    setSearchResult([])
+                }
+            })
+            .catch(error => console.log(error))
     }
-  }
+
+    const handleClose = () => {
+        setSearchResult([])
+        const input = document.querySelector(`#inputField`)
+        input.value = ``
+    }
+
+    const activeBtn = (e) => {
+        document.querySelectorAll(`.header__list-item`)
+            .forEach(item => {
+                item.classList.remove(`active`)
+            })
+
+        if (e.target.tagName === 'LI') {
+            e.target.classList.add(`active`)
+        } else if (e.target.tagName === 'H1') {
+            document.querySelector(`.header__list-item`).classList.add(`active`)
+        }
+    }
 
 
-  return (
-    <header className='header'>
-      <section className='header__container'>
-        <Link to="/" onClick={activeBtn} className='header__linktag' >
-          <h1 className='header__heading'>NAPFLIX</h1>
-        </Link>
-        <div className='search-container'>
-          <section className='search-dropdown'> {
-            searchResult.map((result) => {
-              return (
-                <Link
-                  onClick={handleClose}
-                  className='search-dropdown__link'
-                  key={result.imdbID}
-                  to={`/singlemovie/${result.imdbID}`}>
-                  <p className='search-dropdown__item'>
-                    {
-                      result.Title
-                    }
-                  </p>
+    return (
+        <header className='header'>
+            <section className='header__container'>
+                <Link to="/" onClick={activeBtn} className='header__linktag' >
+                    <h1 className='header__heading'>NAPFLIX</h1>
                 </Link>
-              )
-            })}
-          </section>
-          <input
-            id='inputField'
-            onChange={handleSearchInput}
-            className='header__input'
-            type="text"
-            placeholder='Search...'
-            aria-label='search bar field'
-          />
-          <Link
-            onClick={handleClose}
-            to={`/search/${searchValue}`}
-            className='header__linktag header__btn'
-          >
-            <img
-              className='header__search-icon'
-              src={searchIcon}
-              alt='search icon'
-            />
-          </Link>
-        </div>
-        <nav className='header__nav'>
-          <ul className='header__nav-list'>
-            <Link  to="/" className='header__linktag' >
-              <li onClick={activeBtn} className='header__list-item'>Home</li>
-            </Link>
-            <Link to="/favorites/" className='header__linktag'>
-              <li onClick={activeBtn} className='header__list-item'>Favorites</li>
-            </Link>
-            <Link to="/watchlist/" className='header__linktag'>
-              <li onClick={activeBtn} className='header__list-item'>Watchlist</li>
-            </Link>
-          </ul>
-        </nav>
-      </section>
-    </header>
-  )
+                <div className='search-container'>
+                    <section className='search-dropdown'> {
+                        searchResult.map((result) => {
+                            return (
+                                <Link
+                                    onClick={handleClose}
+                                    className='search-dropdown__link'
+                                    key={result.imdbID}
+                                    to={`/singlemovie/${result.imdbID}`}>
+                                    <p className='search-dropdown__item'>
+                                        {
+                                            result.Title
+                                        }
+                                    </p>
+                                </Link>
+                            )
+                        })}
+                    </section>
+                    <input
+                        id='inputField'
+                        onChange={handleSearchInput}
+                        className='header__input'
+                        type="text"
+                        placeholder='Search...'
+                        aria-label='search bar field'
+                    />
+                    <Link
+                        onClick={handleClose}
+                        to={`/search/${searchValue}`}
+                        className='header__linktag header__btn'
+                    >
+                        <img
+                            className='header__search-icon'
+                            src={searchIcon}
+                            alt='search icon'
+                        />
+                    </Link>
+                </div>
+                <nav className='header__nav'>
+                    <ul className='header__nav-list'>
+                        {/* <Link to="/" className='header__linktag' >
+                            <li onClick={activeBtn} className='header__list-item'>Home</li>
+                        </Link> */}
+                        <Link to="/favorites/" className='header__linktag'>
+                            <li onClick={activeBtn} className='header__list-item'>Favorites</li>
+                        </Link>
+                        <Link to="/watchlist/" className='header__linktag'>
+                            <li onClick={activeBtn} className='header__list-item'>Watchlist</li>
+                        </Link>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+    )
 }
 
 export default Header;
--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -3,53 +3,53 @@ import './movieCard.css'
 import { Link } from 'react-router-dom';
 
 function MovieCard({ movie, favoriteMovies, watchlist, handleFavorites, handleWatchlist }) {
-  const [isInWatchlist, setIsInWatchlist] = useState(false);
-  const [isInFavorites, setIsInFavorites] = useState(false);
+    const [isInWatchlist, setIsInWatchlist] = useState(false);
+    const [isInFavorites, setIsInFavorites] = useState(false);
 
-  useEffect(() => {
-    setIsInWatchlist(watchlist.some(item => item.imdbid === movie.imdbid));
-    setIsInFavorites(favoriteMovies.some(item => item.imdbid === movie.imdbid));
-  }, [watchlist, favoriteMovies, movie]);
+    useEffect(() => {
+        setIsInWatchlist(watchlist.some(item => item.imdbid === movie.imdbid));
+        setIsInFavorites(favoriteMovies.some(item => item.imdbid === movie.imdbid));
+    }, [watchlist, favoriteMovies, movie]);
 
-  const toggleWatchlist = () => {
-    if (isInWatchlist) {
-      handleWatchlist(movie, 'remove');
-      setIsInWatchlist(false);
-    } else {
-      handleWatchlist(movie, 'add');
-      setIsInWatchlist(true);
-    }
-  };
+    const toggleWatchlist = () => {
+        if (isInWatchlist) {
+            handleWatchlist(movie, 'remove');
+            setIsInWatchlist(false);
+        } else {
+            handleWatchlist(movie, 'add');
+            setIsInWatchlist(true);
+        }
+    };
 
-  const toggleFavorites = () => {
-    if (isInFavorites) {
-      handleFavorites(movie, 'remove');
-      setIsInFavorites(false);
-    } else {
-      handleFavorites(movie, 'add');
-      setIsInFavorites(true);
-    }
-  };
+    const toggleFavorites = () => {
+        if (isInFavorites) {
+            handleFavorites(movie, 'remove');
+            setIsInFavorites(false);
+        } else {
+            handleFavorites(movie, 'add');
+            setIsInFavorites(true);
+        }
+    };
 
-  return (
-    <article className='movieCard'>
-      <Link to="//singlemoviepage/:id">
-        <img className='movieCard__poster' src={movie.poster} alt={movie.title} />
-        <section className='movieCard__overlay'>
-          <h2 className='movieCard__title'>{movie.title} </h2>
+    return (
+        <article className='movieCard'>
+            <Link to="//singlemoviepage/:id">
+                <img className='movieCard__poster' src={movie.poster} alt={movie.title} />
+                <section className='movieCard__overlay'>
+                    <h2 className='movieCard__title'>{movie.title} </h2>
 
-          <div className="movieCard__actions">
-            <button className='watchlist-btn' onClick={(e) => { e.preventDefault(); toggleWatchlist() }}>
-              <img src={isInWatchlist ? 'src/assets/check.svg' : 'src/assets/plus.svg'} alt="Watchlist" />
-            </button>
-            <button className='favlist-btn' onClick={(e) => { e.preventDefault(); toggleFavorites() }}>
-              <img src={isInFavorites ? 'src/assets/starFilled.svg' : 'src/assets/StarEmpty.svg'} alt="Favorites" />
-            </button>
-          </div>
-        </section>
-      </Link>
-    </article>
-  );
+                    <div className="movieCard__actions">
+                        <button className='watchlist-btn' onClick={(e) => { e.preventDefault(); toggleWatchlist() }}>
+                            <img src={isInWatchlist ? '../src/assets/checked.svg' : '../src/assets/plus.svg'} alt="Watchlist" />
+                        </button>
+                        <button className='favlist-btn' onClick={(e) => { e.preventDefault(); toggleFavorites() }}>
+                            <img src={isInFavorites ? '../src/assets/starFilled.svg' : '../src/assets/StarEmpty.svg'} alt="Favorites" />
+                        </button>
+                    </div>
+                </section>
+            </Link>
+        </article>
+    );
 }
 
 export default MovieCard;
